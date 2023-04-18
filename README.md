6cec84c7042e371db4ed88d7ea08ee0b59f4cc96
pratiman1 <74183405+pratiman1@users.noreply.github.com>
2023-04-18 21:33:06 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# dogting
-bootstrap
+TinDog Starting Files
---
+++ jv1.js
@@ -0,0 +1,6 @@
+var r1=prompt("Enter  your PET age? ");
+var r2=prompt("Enter your PET love age ? ");
+var r=Math.random();
+var m=r*100;
+var v=m+1;
+alert("Your love score is "+ Math.floor(v)+"%");
